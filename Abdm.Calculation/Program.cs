14dd696d2ea98bb7fdd7896ff0c490abfa5034d4
i.bluvshtein <i.bluvshtein@norddig.ru>
2025-08-14 17:52:18 +0300

    fixed program.cs added dockerfile

--- Abdm.Calculation/Program.cs
+++ Abdm.Calculation/Program.cs
@@ -1,13 +1,32 @@
-namespace Abdm.Calculation
+using Abdm.Calculation.Infrastructure;
+using Abdm.Reports.Calculation.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace Abdm.Reports.Calculation
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            builder.Services.AddLogging(cfg =>
+            {
+                cfg.ClearProviders();
+                cfg.AddConfiguration(builder.Configuration.GetSection("Logging"));
+#if DEBUG
+                cfg.AddDebug();
+#endif
+                cfg.AddNLog();
+            });
+
+            builder.Services.AddServices(builder.Configuration);
+            builder.Services.AddKafka(builder.Configuration);
+
+            var app = builder.Build();
 
             app.Run();
         }
