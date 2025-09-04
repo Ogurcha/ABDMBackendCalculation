e53c7b809a5783ac2fcbfa6e9d23215911e5a3cd
i.bluvshtein <i.bluvshtein@norddig.ru>
2025-09-04 11:31:31 +0300

    ABDMP-313 - Mesh excessive logic removed

--- Abdm.Calculation.Graphics/Entities/Mesh.cs
+++ Abdm.Calculation.Graphics/Entities/Mesh.cs
@@ -12,33 +12,5 @@ namespace Abdm.Calculation.Graphics.Entities
         public MeshData? Data { get; set; }
     }
 
-    /// <summary>
-    /// Вычисляемые данные по кэшу. Здесь происходит денормализация и дублирование во имя оптимизации
-    /// </summary>
-    public class MeshData
-    {
-        public double MinX { get; set; }
-        public double MinY { get; set; }
-        public double MinZ { get; set; }
-        public double MaxX { get; set; }
-        public double MaxY { get; set; }
-        public double MaxZ { get; set; }
-
-        /// <summary>
-        /// Уникальные значения точек по оси Х.
-        /// </summary>
-        public double[]? DistinctXs { get; set; }
-
-        /// <summary>
-        /// Уникальные значения точек по оси Y.
-        /// </summary>
-        public double[]? DistinctYs { get; set; }
-
-        /// <summary>
-        /// Уникальные значения точек по оси Х + с учётом размера колёс
-        /// </summary>
-        public double[]? DistinctXsWithWheels { get; set; }
-
-        bool IsGridRegular { get; set; }
-    }
+    
 }
---
+++ Abdm.Calculation.Graphics/Entities/MeshData.cs
@@ -0,0 +1,23 @@
+﻿namespace Abdm.Calculation.Graphics.Entities
+{
+    /// <summary>
+    /// Вычисляемые данные по кэшу. Здесь происходит денормализация и дублирование во имя оптимизации
+    /// </summary>
+    public class MeshData
+    {
+        /// <summary>
+        /// Уникальные значения точек по оси Х.
+        /// </summary>
+        public double[]? DistinctXs { get; set; }
+
+        /// <summary>
+        /// Уникальные значения точек по оси Y.
+        /// </summary>
+        public double[]? DistinctYs { get; set; }
+
+        /// <summary>
+        /// Уникальные значения точек по оси Х + с учётом размера колёс
+        /// </summary>
+        public double[]? DistinctXsWithWheels { get; set; }
+    }
+}
